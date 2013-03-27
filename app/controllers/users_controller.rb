36370cf6513176988dbfb6f90475d178564f1cc0
Irfan Pirbhai <irfan.pirbhai@gmail.com>
2013-03-27 15:08:52 -0400

    Add User controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to products_path, :notice => "User successfully created!"
+    else
+      render :action => :new
+    end
+
+  end
+end
