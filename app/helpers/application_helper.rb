1074307aa6ebbb0a24f91c4843e6713e88efcc2b
Irfan Pirbhai <irfan.pirbhai@gmail.com>
2013-03-27 11:56:59 -0400

    generate product model, generate products controller, erase the routes, change actions, delete extra views, make index page, make new page, write partial _form, make show page, write flash notice in application.html.erb, move flash code into a partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,3 @@
 module ApplicationHelper
 
-  def show_flash_fields
-
-  if flash[:notice]
-    <div class="notice"><%= flash[:notice] %></div>
-  end
-
-  if flash[:alert]
-    <div class="alert"><%= flash[:alert] %></div>
-  end
-  
-  if flash[:error]
-    <div class="error"><%= flash[:error] %></div>
-  end
-
 end
