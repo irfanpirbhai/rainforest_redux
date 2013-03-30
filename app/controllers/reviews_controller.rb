d650ad873b14ab1a03deb2c1e0a562d1202d1cf8
Irfan Pirbhai <irfan.pirbhai@gmail.com>
2013-03-29 23:17:31 -0400

    Add showing list of reviews on product and ability to create and send if logged in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,12 +7,21 @@ class ReviewsController < ApplicationController
   end
 
   def new
-    @product = Product.find_by_id(params[:product_id])
-    @review = Review.new
   end
 
   def create
-  
+    @product = Product.find_by_id(params[:product_id])
+    @review = Review.new
+    @review.comment = params[:review][:comment]
+    @review.product_id = params[:product_id]
+    @review.user_id = session[:user_id]
+    if @review.save
+      flash[:notice] = "Thanks for the review!"
+      redirect_to @product
+    else 
+      flash[:error] = "There was an error saving your review"
+      redirect_to @product
+    end
   end
 
   def show
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   attr_accessible :comment
 
-  validates :comment, :presence => true
+  validates :comment, :user_id, :product_id, :presence => true
 
   belongs_to :product
   belongs_to :user
